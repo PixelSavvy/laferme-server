870981802e103b378020c88ee0d7cc0af0b39792
PixelSavvy <web.pixelwork@gmail.com>
2024-12-17 11:40:27 +0100

    update app client origin

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://laferme-client.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE');
   res.setHeader(
     'Access-Control-Allow-Headers',
