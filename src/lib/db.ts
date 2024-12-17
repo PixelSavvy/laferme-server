a1ad03c4b75b8b950e992c4afc2ccb1be7b32577
PixelSavvy <web.pixelwork@gmail.com>
2024-12-17 11:13:57 +0100

    connect to render db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,7 +7,7 @@ import { Sequelize } from 'sequelize';
 const environment = process.env.NODE_ENV || 'development';
 const configEnv = dbConfig[environment as keyof Config];
 
-const sequelize = new Sequelize(process.env.DB_URL!, {
+const sequelize = new Sequelize(process.env.DB_URL as string, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
